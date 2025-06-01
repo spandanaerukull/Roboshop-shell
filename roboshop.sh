4c439857ed202ae98105f61e9e0a19eb4b662bf7
spandana <spandu@devops.com>
2025-06-01 04:12:57 -0500

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -10,8 +10,8 @@ DOMAIN_NAME="spandanas.click" # replace with your domain name
 for instance in ${INSTANCES[@]}
 do 
 
-       INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-081d2f048f93f433e --tag-specifications "ResourceType=instance,
-       Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+       INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.
+       micro --security-group-ids sg-081d2f048f93f433e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
                   if [ $instance != "frontend" ]
                   then
                     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
@@ -21,5 +21,5 @@ do
                         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
                     Instances[0].PublicIpAddress" --output text)
                     fi
-                    echo "$INSTANCE IP address: $IP"
-done
+                    echo "$instance IP address: $IP"
+                 done
