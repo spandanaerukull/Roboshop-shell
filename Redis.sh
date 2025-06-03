ec1c9dfbf74b85025b2f98a8a6fc79b4a00511b7
spandana <spandu@devops.com>
2025-06-03 06:44:01 -0500

    redis.sh

---
+++ Redis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R=\e[31m"
+G=\e[32m"
+Y=\e[33m"
+N=\e[0m"
+LOGS_FOLDER="/var/log/roboshop"
+SCRIPT_NAME=$(echo $0 | cut -d "," -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo -e "Script started executing at: $(date)" |tee -a $LOG_FILE
+
+#check the user has root privileges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N" |tee -a $LOG_FILE 
+    exit 1 # give other than 0  up to 127
+else  
+    echo -e " You are running with root access " |tee -a $LOG_FILE
+fi
+
+#validate function takes inputs as exit statis, what command they tried to install
+VALIDATE() {
+    if [ $1 -eq 0 ]
+    then
+        echo -e " $2 is ...$G success $N" |tee -a $LOG_FILE
+    else
+        echo -e " $2 is ..$R failure $N" |tee -a $LOG_FILE
+    exit 1
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling default Redis"
+
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing Redis 7"
+
+
