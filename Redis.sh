4b69c904a53f3a55f47034159018ae46c66a549b
spandana <spandu@devops.com>
2025-06-04 03:09:11 -0500

    redis.sh

--- Redis.sh
+++ Redis.sh
@@ -43,5 +43,5 @@ VALIDATE $? "Enabling Redis 7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g'  -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Editing Redis conf files for remote connections"
+#ed -i 's/127.0.0.1/0.0.0.0/g'  -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+#ALIDATE $? "Editing Redis conf files for remote connections"
