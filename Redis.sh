b80bde7ba67c8013a4963512051471d286d25b94
spandana <spandu@devops.com>
2025-06-04 03:43:12 -0500

    redis.sh

--- Redis.sh
+++ Redis.sh
@@ -43,5 +43,17 @@ VALIDATE $? "Enabling Redis 7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
-#ed -i 's/127.0.0.1/0.0.0.0/g'  -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-#ALIDATE $? "Editing Redis conf files for remote connections"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no/' /etc/redis/redis.conf &>>$LOG_FILE                                                
+VALIDATE $? "Edited Redis.conf to accept remote connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis service"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$((END_TIME - START_TIME))
+
+echo -e "Script  exection completed successfully, $Y time taken: $TOTAL_Time $N" | tee -a $LOG_FILES
+
