61612b48f06241f3f66ba7b51e699bad2e83e5e8
spandana <spandu@devops.com>
2025-06-05 05:36:48 -0500

    cart.sh

--- cart.sh
+++ cart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 START_TIME=$(date +%s)
 USERID=$(id -u)
-R=\e[31m"
-G=\e[32m"
-Y=\e[33m"
-N=\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop"
 SCRIPT_NAME=$(echo $0 | cut -d "," -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
